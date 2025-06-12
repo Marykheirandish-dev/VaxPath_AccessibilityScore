fa1e0bc34b33ecf09b18631c0addc9b62a2420a2
Marykheirandish-dev <marykheirandish69@gmail.com>
2025-06-12 16:48:51 -0400

    Update API key in config and standardize string quotes

--- src/config.py
+++ src/config.py
@@ -1,3 +1,4 @@
+# --- LIBRARIES
 import time
 
 import folium
@@ -10,7 +11,7 @@ from geopy.distance import geodesic
 from openrouteservice.exceptions import ApiError
 
 # --- CONFIG ---
-ORS_KEY = 'your API key here'
+ORS_KEY = "5b3ce3597851110001cf62489b5896d9ce48408784b8cd929b44c9b0"
 TRAVEL_TIME_THRESHOLD = 30  # in minutes
 DISTANCE_FILTER_MILES = 35  # skip distant locations
-POP_COLUMN = 'population'   # adjust if needed
+POP_COLUMN = "population"  # adjust if needed
---
+++ src/dataset.py
@@ -0,0 +1,3 @@
+# --- LOAD DATA ---
+zcta_df = pd.read_csv(r"C:\zcta_score_data_1st.csv")
+provider_df = pd.read_csv(r"C:\vfc_providers_geocoded.csv")
